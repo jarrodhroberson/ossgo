8a73940749e8bc80d5e291c1d134e161523447a0
Jarrod Roberson <jarrod@vertigrated.com>
2024-07-06 15:24:31 -0400

    adding subscription sync functions

--- firestore/functions.go
+++ firestore/functions.go
@@ -6,11 +6,12 @@ import (
 	"os"
 	"strings"
 
-	"cloud.google.com/go/firestore"
-	"cloud.google.com/go/firestore/apiv1/firestorepb"
+	fs "cloud.google.com/go/firestore"
+	fspb "cloud.google.com/go/firestore/apiv1/firestorepb"
 	errs "github.com/jarrodhroberson/ossgo/errors"
 	"github.com/joomcode/errorx"
 	"github.com/rs/zerolog/log"
+	"google.golang.org/api/iterator"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
@@ -19,7 +20,40 @@ func IsDocumentNotFound(err error) bool {
 	return status.Code(err) == codes.NotFound
 }
 
-func Must(client *firestore.Client, err error) *firestore.Client {
+func DeleteCollection(ctx context.Context, client *fs.Client, path string) error {
+	colRef := client.Collection(path)
+	bulkwriter := client.BulkWriter(ctx)
+	defer bulkwriter.End()
+
+	for {
+		iter := colRef.Limit(500).Documents(ctx)
+		numDeleted := 0
+		for {
+			doc, err := iter.Next()
+			if err == iterator.Done {
+				break
+			}
+			if err != nil {
+				return err
+			}
+
+			_, err = bulkwriter.Delete(doc.Ref)
+			if err != nil {
+				return err
+			}
+			numDeleted++
+		}
+
+		if numDeleted == 0 {
+			break
+		}
+
+		bulkwriter.Flush()
+	}
+	return nil
+}
+
+func Must(client *fs.Client, err error) *fs.Client {
 	if err != nil {
 		log.Error().Err(err).Msgf("error creating firestore client %s", err)
 		panic(err)
@@ -28,11 +62,11 @@ func Must(client *firestore.Client, err error) *firestore.Client {
 	}
 }
 
-func Client(ctx context.Context, database DatabaseName) (*firestore.Client, error) {
+func Client(ctx context.Context, database DatabaseName) (*fs.Client, error) {
 	if strings.Trim(string(database), " ") == "" {
 		return nil, errorx.IllegalArgument.New("DatabaseName can not be an empty string")
 	}
-	client, err := firestore.NewClientWithDatabase(ctx, os.Getenv("GOOGLE_CLOUD_PROJECT"), string(database))
+	client, err := fs.NewClientWithDatabase(ctx, os.Getenv("GOOGLE_CLOUD_PROJECT"), string(database))
 	if err != nil {
 		log.Fatal().Err(err).Msgf("error creating firestore client %s", err)
 		return nil, err
@@ -40,7 +74,7 @@ func Client(ctx context.Context, database DatabaseName) (*firestore.Client, erro
 	return client, nil
 }
 
-func Count(ctx context.Context, query firestore.Query) int64 {
+func Count(ctx context.Context, query fs.Query) int64 {
 	cq := query.NewAggregationQuery().WithCount("val")
 	cqr, err := cq.Get(ctx)
 	if err != nil {
@@ -49,16 +83,15 @@ func Count(ctx context.Context, query firestore.Query) int64 {
 	}
 	value, ok := cqr["val"]
 	if !ok {
-		log.Fatal().Err(err).Msg("could not get \"all\" alias for count")
+		log.Fatal().Err(err).Msg("could not get \"val\" alias for count")
 		return -1
 	}
-	val := value.(*firestorepb.Value)
-	return val.GetIntegerValue()
+	return value.(*fspb.Value).GetIntegerValue()
 }
 
 func GetAs[T any](ctx context.Context, database DatabaseName, path string, t *T) error {
 	client := Must(Client(ctx, database))
-	defer func(client *firestore.Client) {
+	defer func(client *fs.Client) {
 		err := client.Close()
 		if err != nil {
 			log.Error().Err(err).Msgf("error closing firestore client %s", err)
