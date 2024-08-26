4a5b0b857e4c426d42274d0616b040395907a4e1
Jarrod Roberson <jarrod@vertigrated.com>
2024-08-26 10:53:18 -0400

    adding enhanced net/url functions.go

--- net/url/functions.go
+++ net/url/functions.go
@@ -4,7 +4,16 @@ import (
 	neturl "net/url"
 )
 
-func MustJoin(url neturl.URL, elems ...string) string {
+func MustParse(rawURL string) *neturl.URL {
+	if u, err := neturl.Parse(rawURL); err != nil {
+		panic(err)
+	} else {
+		return u
+	}
+
+}
+
+func MustJoin(url *neturl.URL, elems ...string) string {
 	if s, err := neturl.JoinPath(url.String(), elems...); err != nil {
 		panic(err)
 	} else {
