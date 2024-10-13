148b4d3878e9ef0747a565a16db9fd8efdd7a6fd
Jarrod Roberson <jarrod@vertigrated.com>
2024-10-12 23:27:38 -0400

    moving identityplatform client code to here

--- gcp/identityplatform.go
+++ gcp/identityplatform.go
@@ -10,6 +10,24 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
+/*
+IdentityToolkitAccountSignUpError
+This is an example of the payload when there is an error from the IdentityToolkit
+
+	<code>{
+	  "error": {
+	    "code": 400,
+	    "message": "EMAIL_EXISTS",
+	    "errors": [
+	      {
+	        "message": "EMAIL_EXISTS",
+	        "domain": "global",
+	        "reason": "invalid"
+	      }
+	    ]
+	  }
+	}</code>
+*/
 type IdentityToolkitAccountSignUpError struct {
 	Error struct {
 		Code   int `json:"code"`
@@ -31,7 +49,11 @@ type SignUpWithEmailPasswordResponse struct {
 	Error        IdentityToolkitAccountSignUpError `json:"error"`
 }
 
-type IdentityPlatform struct {
+func NewIdentityPlatformClient() *Client {
+	return &Client{
+		host:   "identitytoolkit.googleapis.com",
+		apiKey: secrets.GetSecretValueAsString(context.Background(), "IDENTITY_PLATFORM_API_KEY"), //TODO: make this dynamic otherwise it will require server restarts to change it.
+	}
 }
 
 type Client struct {
@@ -39,11 +61,44 @@ type Client struct {
 	apiKey string
 }
 
-func NewIdentityPlatformClient() *Client {
-	return &Client{
-		host:   "identitytoolkit.googleapis.com",
-		apiKey: secrets.GetSecretValueAsString(context.Background(), "IDENTITY_PLATFORM_API_KEY"), //TODO: make this dynamic otherwise it will require server restarts to change it.
+func (ipc *Client) VerifyIdToken(ctx context.Context, token string) (*auth.Token, error) {
+	app, err := fb.NewApp(context.Background(), nil)
+	if err != nil {
+		err = errorx.InitializationFailed.Wrap(err, "error initialising firebase app")
+		log.Fatal().Err(err).Msg(err.Error())
 	}
+	authClient, err := app.Auth(ctx)
+	if err != nil {
+		err = errorx.InitializationFailed.Wrap(err, "error initialising firebase auth client")
+		log.Fatal().Err(err).Msg(err.Error())
+		return nil, err
+	}
+	return authClient.VerifyIDToken(ctx, token)
+}
+
+/*
+GetUserData
+endpoint https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=[API_KEY]
+documentation https://cloud.google.com/identity-platform/docs/use-rest-api#section-get-account-info
+common error codes: INVALID_ID_TOKEN, USER_NOT_FOUND
+*/
+func (ipc *Client) GetUserData(ctx context.Context, idToken string) (*auth.UserRecord, error) {
+	app, err := fb.NewApp(context.Background(), nil)
+	if err != nil {
+		err = errorx.InitializationFailed.Wrap(err, "error initialising firebase app")
+		log.Fatal().Err(err).Msg(err.Error())
+	}
+	authClient, err := app.Auth(ctx)
+	if err != nil {
+		err = errorx.InitializationFailed.Wrap(err, "error initialising firebase auth client")
+		log.Fatal().Err(err).Msg(err.Error())
+		return nil, err
+	}
+	token, err := ipc.VerifyIdToken(ctx, idToken)
+	if err != nil {
+		return nil, err
+	}
+	return authClient.GetUser(ctx, token.UID)
 }
 
 func (ipc *Client) GetByEmailAddress(ctx context.Context, emailAddress string) (*auth.UserRecord, error) {
