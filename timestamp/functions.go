0dea3d404ba156a2241ed807ccba1b7e310b201c
Jarrod Roberson <jarrod@vertigrated.com>
2025-01-22 12:22:07 -0500

    adding convience function to parse youtube v3 api specific formatted timestamps

--- timestamp/functions.go
+++ timestamp/functions.go
@@ -37,6 +37,11 @@ func AddMonth(ts Timestamp, m int) Timestamp {
 	return From(addMonth(ts.t, m))
 }
 
+// ParseYouTubeTimestamp parses a non-standard timestamp format that the YouTube V3 API uses.
+func ParseYouTubeTimestamp(s string) Timestamp {
+	return MustParse("2006-01-02T15:04:05.999999Z", s)
+}
+
 func MonthToPeriod(ts Timestamp) Period {
 	firstDayOfMonth := From(time.Date(ts.Year(), ts.Month(), 1, 0, 0, 0, 0, time.UTC))
 	duration := time.Duration(24 * 7 * daysIn(ts.Month(), ts.Year()))
