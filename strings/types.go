8e997872e26f94b7c6ae863f13aac2d7529ad652
Jarrod Roberson <jarrod@vertigrated.com>
2025-02-12 20:56:45 -0500

    Revert "managing redis memory store in gcp"
    This reverts commit f5aaeedd1fc3251b77df889e712ae3f62bbb4bb0.

--- strings/types.go
+++ strings/types.go
@@ -1,10 +1,14 @@
 package strings
 
+import (
+	"unique"
+)
+
 /*
 NO_DATA is used as a marker value to show that no data was provided
 and that the data is not just missing.
 It is represented as unicode character NULL (\u0000)
 */
-var NO_DATA = "\u0000"
+var NO_DATA = unique.Make("\u0000")
 
 type Stringifier[T any] func(t T) string
