c922bf19407a3edecff953082a5e6d1643cb1931
Jarrod Roberson <jarrod@vertigrated.com>
2025-02-12 20:56:26 -0500

    Revert "refactoring interface to accept and return pointers"
    This reverts commit ea15d3013195cc41229f93d7cb33400d57090a67.

--- persistence/types.go
+++ persistence/types.go
@@ -5,7 +5,7 @@ import (
 )
 
 type Repository[T any] interface {
-	Load(ctx context.Context, key string) (*T, error)
-	Store(ctx context.Context, key string, value *T) error
+	Load(ctx context.Context, key string) (T, error)
+	Store(ctx context.Context, key string, value T) error
 	Remove(ctx context.Context, key string) error
 }
