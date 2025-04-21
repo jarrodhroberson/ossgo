fbc5620163e0890efd23717e5ed7317683736c5e
Jarrod Roberson <jarrod@vertigrated.com>
2025-04-20 20:07:47 -0400

    moving boilerplate CreateTask to here.

--- tasks/functions.go
+++ tasks/functions.go
@@ -1,12 +1,16 @@
 package tasks
 
 import (
+	"context"
 	"fmt"
 
+	cloudtasks "cloud.google.com/go/cloudtasks/apiv2"
 	"cloud.google.com/go/cloudtasks/apiv2/cloudtaskspb"
 
+	errs "github.com/jarrodhroberson/ossgo/errors"
 	"github.com/jarrodhroberson/ossgo/functions/must"
 	"github.com/jarrodhroberson/ossgo/gcp"
+	"github.com/rs/zerolog/log"
 )
 
 func CreateQueuePath(queueId string) string {
@@ -20,4 +24,27 @@ func DisallowDuplicates(path string, name string) CreateTaskRequestOption {
 		// Task name must be formatted: "projects/<PROJECT_ID>/locations/<LOCATION_ID>/queues/<QUEUE_ID>/tasks/<TASK_ID>"
 		ctr.Task.Name = fmt.Sprintf("%s/tasks/%s", path, name)
 	}
+}
+
+func CreateTask(ctx context.Context, req *cloudtaskspb.CreateTaskRequest) (*cloudtaskspb.Task, error) {
+	client, err := cloudtasks.NewClient(ctx)
+	if err != nil {
+		return nil, err
+	}
+	defer func(c *cloudtasks.Client) {
+		err = c.Close()
+		if err != nil {
+			log.Error().Err(err).Msg(err.Error())
+		}
+	}(client)
+
+	createdTask, err := client.CreateTask(ctx, req)
+	if err != nil {
+		if err.Error() == "AlreadyExists" {
+			return nil, errs.NotCreatedError.Wrap(err, "task already exists %s", req.Task.Name)
+		} else {
+			return nil, fmt.Errorf("cloudtasks.CreateTask: %v", err)
+		}
+	}
+	return createdTask, nil
 }
