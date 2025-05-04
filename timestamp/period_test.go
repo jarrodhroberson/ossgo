5f8617ed50e9e20e0374853ecc5ab3fa2b503866
Jarrod Roberson <jarrod@vertigrated.com>
2025-05-04 10:57:01 -0400

    removing unused error

---
+++ timestamp/period_test.go
@@ -0,0 +1,40 @@
+package timestamp
+
+import (
+	"reflect"
+	"testing"
+	"time"
+)
+
+func TestDayToPeriod(t *testing.T) {
+	type args struct {
+		ts *Timestamp
+	}
+	tests := []struct {
+		name string
+		args args
+		want *Period
+	}{
+		{
+			name: "test_with_valid_timestamp",
+			args: args{
+				ts: &Timestamp{
+					t: time.Date(2023, 10, 5, 0, 0, 0, 0, time.UTC),
+				},
+			},
+			want: &Period{
+				Start: &Timestamp{t: time.Date(2023, 10, 5, 0, 0, 0, 0, time.UTC)},
+				End: &Timestamp{
+					t: time.Date(2023, 10, 6, 0, 0, 0, 0, time.UTC),
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := DayToPeriod(tt.args.ts); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("DayToPeriod() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
