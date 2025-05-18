67de2e7327690f423b14693f9309b79df84cac26
Jarrod Roberson <jarrod@vertigrated.com>
2025-05-18 16:47:27 -0400

    adding unicode based TrimFunc for strings.TrimFunc

--- strings/functions.go
+++ strings/functions.go
@@ -36,27 +36,9 @@ func Join[S fmt.Stringer](it iter.Seq[S], sep string) string {
 }
 
 func IsUnicodePrintable(r rune) bool {
-	if unicode.IsControl(r) {
-		return false // Exclude control characters
-	}
-	if unicode.Is(unicode.Mn, r) || unicode.Is(unicode.Me, r) || unicode.Is(unicode.Mc, r) {
-		return false // Exclude nonspacing marks, enclosing marks, and combining marks.
-	}
-	if unicode.Is(unicode.Zs, r) {
-		return true // Keep spaces
-	}
-	if unicode.Is(unicode.Zl, r) || unicode.Is(unicode.Zp, r) {
-		return false // Exclude line and paragraph separators.
-	}
-	if unicode.Is(unicode.Cs,r) || unicode.Is(unicode.Co, r) {
-		return false // Exclude surrogates and private use
-	}
-
-	// Include everything else as printable
-	return true
+	return !IsControl(IsOtherSurrogate(IsOtherPrivateUse(IsSepAny(func(r rune) bool { return false }))))(r)
 }
 
-//
 // IsStringUnicodePrintable checks if all runes in a given string `s` are considered printable according to the Unicode standard.
 //
 // The function iterates through each rune in the string using a sequence generator (`seq.RuneSeq`)
@@ -69,7 +51,6 @@ func IsUnicodePrintable(r rune) bool {
 //
 // Returns:
 //   - bool: Returns true if all runes in the string are printable; otherwise, returns false.
-//
 func IsStringUnicodePrintable(s string) bool {
 	for r := range seq.RuneSeq(s) {
 		if !IsUnicodePrintable(r) {
@@ -77,4 +58,153 @@ func IsStringUnicodePrintable(s string) bool {
 		}
 	}
 	return true
+}
+
+// IsPrintable creates a RunePredicate that checks if a rune is not printable by combining multiple Unicode checks.
+// It chains together several predicates to check for control characters, marks, line separators,
+// paragraph separators, private use characters, and surrogate code points.
+//
+// Parameters:
+//   - next: The next RunePredicate in the chain to be evaluated if none of the non-printable checks match
+//
+// Returns:
+//   - RunePredicate: A function that returns true if the rune is not printable or if the next predicate returns true
+func IsPrintable(next RunePredicate) RunePredicate {
+	return func(r rune) bool {
+		return IsLetter(IsNumber(IsPunct(IsSymbol(IsSpace(next)))))(r)
+	}
+}
+
+func IsEmoji(next RunePredicate) RunePredicate {
+	return func(r rune) bool {
+		return unicode.Is(unicode.So, r) || next(r)
+	}
+}
+
+// IsOtherPrivateUse creates a RunePredicate that checks if a rune is a private use character (Co),
+// or matches the next predicate.
+func IsOtherPrivateUse(next RunePredicate) RunePredicate {
+	return func(r rune) bool {
+		return unicode.Is(unicode.Co, r) || next(r)
+	}
+}
+
+// IsOtherSurrogate creates a RunePredicate that checks if a rune is a surrogate character (Cs),
+// or matches the next predicate.
+func IsOtherSurrogate(next RunePredicate) RunePredicate {
+	return func(r rune) bool {
+		return unicode.Is(unicode.Cs, r) || next(r)
+	}
+}
+
+// IsSepAny creates a RunePredicate that checks if a rune is any type of separator (spaces, lines, paragraphs),
+// by chaining together IsSepSpace, IsSepLine, and IsSepParagraph predicates.
+//
+// Parameters:
+//   - next: The next RunePredicate in the chain to be evaluated if none of the separators checks match
+//
+// Returns:
+//   - RunePredicate: A function that returns true if the rune is any type of separator or if the next predicate returns true
+func IsSepAny(next RunePredicate) RunePredicate {
+	return IsSepSpace(IsSepLine(IsSepParagraph(next)))
+}
+
+// IsSepSpace creates a RunePredicate that checks if a rune is a space separator (Zs),
+// or matches the next predicate.
+//
+// Parameters:
+//   - next: The next RunePredicate in the chain to be evaluated if the space separator check doesn't match
+//
+// Returns:
+//   - RunePredicate: A function that returns true if the rune is a space separator or if the next predicate returns true
+func IsSepSpace(next RunePredicate) RunePredicate {
+	return func(r rune) bool {
+		return unicode.Is(unicode.Zs, r) || next(r)
+	}
+}
+
+// IsSepLine creates a RunePredicate that checks if a rune is a line separator (Zl),
+// or matches the next predicate.
+func IsSepLine(next RunePredicate) RunePredicate {
+	return func(r rune) bool {
+		return unicode.Is(unicode.Zl, r) || next(r)
+	}
+}
+
+// IsSepParagraph creates a RunePredicate that checks if a rune is a paragraph separator (Zp),
+// or matches the next predicate.
+func IsSepParagraph(next RunePredicate) RunePredicate {
+	return func(r rune) bool {
+		return unicode.Is(unicode.Zp, r) || next(r)
+	}
+}
+
+// IsControl creates an IsRune that checks if a rune is a control character (Cc).
+func IsControl(next RunePredicate) RunePredicate {
+	return func(r rune) bool {
+		return unicode.Is(unicode.Cc, r) || next(r)
+	}
+}
+
+// IsMark creates an IsRune that checks if a rune is a mark character (Mc, Me, Mn).
+func IsMark(next RunePredicate) RunePredicate {
+	return func(r rune) bool {
+		return unicode.Is(unicode.M, r) || next(r)
+	}
+}
+
+// IsLetter creates an IsRune that checks if a rune is a letter character (Lu, Ll, Lt, Lm, Lo).
+func IsLetter(next RunePredicate) RunePredicate {
+	return func(r rune) bool {
+		return unicode.Is(unicode.L, r) || next(r)
+	}
+}
+
+// IsNumber creates an IsRune that checks if a rune is a number character (Nd, Nl, No).
+func IsNumber(next RunePredicate) RunePredicate {
+	return func(r rune) bool {
+		return unicode.Is(unicode.N, r) || next(r)
+	}
+}
+
+// IsPunct creates an IsRune that checks if a rune is a punctuation character (Pc, Pd, Ps, Pe, Pi, Pf, Po).
+func IsPunct(next RunePredicate) RunePredicate {
+	return func(r rune) bool {
+		return unicode.Is(unicode.P, r) || next(r)
+	}
+}
+
+// IsSymbol creates an IsRune that checks if a rune is a symbol character (Sm, Sc, Sk, So).
+func IsSymbol(next RunePredicate) RunePredicate {
+	return func(r rune) bool {
+		return unicode.Is(unicode.S, r) || next(r)
+	}
+}
+
+// IsSpace creates an IsRune that checks if a rune is a space character (Zs, Zl, Zp).
+func IsSpace(next RunePredicate) RunePredicate {
+	return func(r rune) bool {
+		return unicode.IsSpace(r) || next(r)
+	}
+}
+
+// IsGraphic creates an IsRune that checks if a rune is a graphic character (includes letters, marks, numbers, punctuation, symbols, and spaces).
+func IsGraphic(next RunePredicate) RunePredicate {
+	return func(r rune) bool {
+		return unicode.IsGraphic(r) || next(r)
+	}
+}
+
+// IsPrint creates an IsRune that checks if a rune is a printable character (includes letters, marks, numbers, punctuation, symbols, spaces, and some control characters).
+func IsPrint(next RunePredicate) RunePredicate {
+	return func(r rune) bool {
+		return unicode.IsPrint(r) || next(r)
+	}
+}
+
+// IsOneOf creates an IsRune that checks if a rune belongs to any of the given unicode ranges.
+func IsOneOf(tables []*unicode.RangeTable, next RunePredicate) RunePredicate {
+	return func(r rune) bool {
+		return unicode.IsOneOf(tables, r) || next(r)
+	}
 }
--- strings/types.go
+++ strings/types.go
@@ -8,3 +8,7 @@ It is represented as unicode character NULL (\u0000)
 var NO_DATA = "\u0000"
 
 type Stringifier[T any] func(t T) string
+
+
+// RunePredicate is a function that takes a rune and returns a boolean.
+type RunePredicate func(r rune) bool
